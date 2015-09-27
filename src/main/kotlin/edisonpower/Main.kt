f14f9b525cf4bc323d64f081d5ca13c2f1ff091b
ligi <ligi@ligi.de>
2015-09-27 04:26:44 +0200

    Sound when switching / prepare file logging

--- src/main/kotlin/edisonpower/Main.kt
+++ src/main/kotlin/edisonpower/Main.kt
@@ -3,12 +3,19 @@ package edisonpower
 import org.ligi.plughub.EdiMaxCommands
 import org.ligi.plughub.EdiMaxCommunicator
 import org.ligi.plughub.EdiMaxConfig
+import java.io.File
+import java.lang.Double
 
 val cfg = EdiMaxConfig()
 val comm = EdiMaxCommunicator(cfg)
+val version = "0.1"
+val logDir = File("logs")
+var i = 0
+var switchedOn = false
 
 fun main(args: Array<String>) {
-    println("Meter Edison connecting to" + cfg.host)
+    printLCD("Meter Edison ${version}", "to " + cfg.host)
+    println("Meter Edison connecting to " + cfg.host)
     poll()
 }
 
@@ -18,9 +25,25 @@ fun poll() {
         val nowPower = EdiMaxCommands.unwrapNowPower(response.toString())
         println("response:" + nowPower)
 
-        val runtime = Runtime.getRuntime()
-        runtime.exec("/home/root/bin/rgb-lcd.py 0 0 255 1 " + nowPower)
+        val switchedOnNew = Double.parseDouble(nowPower) < 0.1
 
+        if (switchedOn != switchedOnNew) {
+            beep()
+            switchedOn = switchedOnNew
+        }
+        printLCD(nowPower + "W", "" + i)
+        i++
         poll() // loop
+
     })
+}
+
+private fun printLCD(line1: String?, line2: String?) {
+    val runtime = Runtime.getRuntime()
+    runtime.exec("/home/root/bin/rgb-lcd.py 0 0 255 \"${line1}\" \"${line2}\" ")
+}
+
+private fun beep() {
+    val runtime = Runtime.getRuntime()
+    runtime.exec("/home/root/bin/buzzer.py")
 }
