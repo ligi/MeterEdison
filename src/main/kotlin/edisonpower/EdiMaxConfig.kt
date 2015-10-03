9725ad466b9fe7c5ed29f3725ff964f971990915
ligi <ligi@ligi.de>
2015-10-03 22:02:50 +0200

    Change IP

--- src/main/kotlin/edisonpower/EdiMaxConfig.kt
+++ src/main/kotlin/edisonpower/EdiMaxConfig.kt
@@ -1,6 +1,6 @@
 package org.ligi.plughub
 
-public class EdiMaxConfig (host: String = "192.168.43.121", port: Int = 10000, pass: String = "1234") {
+public class EdiMaxConfig (host: String = "192.168.2.110", port: Int = 10000, pass: String = "1234") {
     public var port: Int? = port
     public var pass: String? = pass
     public var host: String? = host
